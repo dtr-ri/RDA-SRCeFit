c179283f0a0baa4fce3167296648dd5ffcdc6a38
DT <DT@DT-PC>
2024-06-16 00:09:59 +0200

    +izmjena termina

--- SRCeFit/src/srcefit/DLG_Izmjena_Termin.java
+++ SRCeFit/src/srcefit/DLG_Izmjena_Termin.java
@@ -21,13 +21,13 @@ import java.awt.event.ActionEvent;
 import java.sql.Connection;
 import javax.swing.JComboBox;
 
-public class DLG_Izmjena_Aktivnost extends JDialog {
+public class DLG_Izmjena_Termin extends JDialog {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
-	JComboBox comboBoxTeren;
-	JComboBox comboBoxAktivnost;
-	private JTextField textField_aktivnost_naziv;
+	private JTextField textField_termin_pocetak;
+	private JTextField textField_termin_trajanje;
+	JComboBox comboBoxTermin;
 
 
 	/**
@@ -35,7 +35,7 @@ public class DLG_Izmjena_Aktivnost extends JDialog {
 	 */
 	public static void main(String[] args) {
 		try {
-			DLG_Izmjena_Aktivnost dialog = new DLG_Izmjena_Aktivnost();
+			DLG_Izmjena_Termin dialog = new DLG_Izmjena_Termin();
 			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 			dialog.setVisible(true);
 		} catch (Exception e) {
@@ -46,56 +46,56 @@ public class DLG_Izmjena_Aktivnost extends JDialog {
 	/**
 	 * Create the dialog.
 	 */
-	public DLG_Izmjena_Aktivnost() {
+	public DLG_Izmjena_Termin() {
 
 		setBounds(100, 100, 450, 300);
 		setBackground(new Color(255, 255, 255));
 		
-		setTitle("SRCeFIT Izmjena postojeće aktivnosti");
+		setTitle("SRCeFIT Unos novog termina");
 		setBounds(100, 100, 611, 398);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBackground(new Color(240, 255, 240));
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
-				{
-			JLabel Izmjena_aktivnost_naslov = new JLabel("Koju aktivnost želite mijenjati?");
-			Izmjena_aktivnost_naslov.setBounds(10, 10, 413, 22);
-			contentPanel.add(Izmjena_aktivnost_naslov);
+		{
+			JLabel Novi_termin_pocetak = new JLabel("Novi termin početak (sati)");
+			Novi_termin_pocetak.setBounds(10, 120, 122, 22);
+			contentPanel.add(Novi_termin_pocetak);
 		}
 		{
-			JLabel Izmjena_aktivnost_izmjena = new JLabel("Unesite nove podatke:");
-			Izmjena_aktivnost_izmjena.setBounds(10, 88, 413, 22);
-			contentPanel.add(Izmjena_aktivnost_izmjena);
+			JLabel Novi_termin_trajanje = new JLabel("Novi termin trajanje (sati)");
+			Novi_termin_trajanje.setBounds(10, 153, 122, 13);
+			contentPanel.add(Novi_termin_trajanje);
+		}
+
+		{
+			textField_termin_pocetak = new JTextField();
+			textField_termin_pocetak.setBounds(178, 120, 136, 19);
+			contentPanel.add(textField_termin_pocetak);
+			textField_termin_pocetak.setColumns(10);
 		}
 		{
-			JLabel Nova_aktivnost_naziv = new JLabel("Nova aktivnost naziv");
-			Nova_aktivnost_naziv.setBounds(10, 120, 122, 22);
-			contentPanel.add(Nova_aktivnost_naziv);
+			textField_termin_trajanje = new JTextField();
+			textField_termin_trajanje.setBounds(178, 149, 136, 19);
+			contentPanel.add(textField_termin_trajanje);
+			textField_termin_trajanje.setColumns(10);
 		}
 		{
-			JLabel Nova_aktivnost_teren = new JLabel("Nova kativnost koristi teren");
-			Nova_aktivnost_teren.setBounds(10, 153, 158, 13);
-			contentPanel.add(Nova_aktivnost_teren);
+			JLabel Izmjena_termin_naslov = new JLabel("Koji termin želite mijenjati?");
+			Izmjena_termin_naslov.setBounds(10, 10, 413, 22);
+			contentPanel.add(Izmjena_termin_naslov);
 		}
 		{
-			textField_aktivnost_naziv = new JTextField();
-			textField_aktivnost_naziv.setBounds(178, 120, 136, 19);
-			contentPanel.add(textField_aktivnost_naziv);
-			textField_aktivnost_naziv.setColumns(10);
+			JLabel Izmjena_termin_izmjena = new JLabel("Unesite nove podatke:");
+			Izmjena_termin_izmjena.setBounds(10, 88, 279, 22);
+			contentPanel.add(Izmjena_termin_izmjena);
 		}
 		{
-			comboBoxTeren = new JComboBox();
-			comboBoxTeren.setBounds(178, 149, 252, 21);
-			contentPanel.add(comboBoxTeren);
-		}
-		{
-			comboBoxAktivnost = new JComboBox();
-			comboBoxAktivnost.setBounds(10, 36, 420, 21);
-			contentPanel.add(comboBoxAktivnost);
-		}
-		
-		
+			comboBoxTermin = new JComboBox();
+			comboBoxTermin.setBounds(10, 37, 420, 21);
+			contentPanel.add(comboBoxTermin);
+		}	
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
@@ -105,35 +105,34 @@ public class DLG_Izmjena_Aktivnost extends JDialog {
 				okButton.addActionListener(new ActionListener() {
 					@SuppressWarnings("deprecation")
 					public void actionPerformed(ActionEvent e) {
-						MapiranjeBaze_Aktivnost aktivnost = (MapiranjeBaze_Aktivnost) comboBoxAktivnost.getSelectedItem();
-						int idAktivnost_sifra = aktivnost.getAktivnost_sifra();
+						MapiranjeBaze_Termin termin = (MapiranjeBaze_Termin) comboBoxTermin.getSelectedItem();
+						int idTermin_redni_broj = termin.getTermin_redni_broj();
 						
-						String Nova_aktivnost_naziv = textField_aktivnost_naziv.getText();
-						
-						MapiranjeBaze_Teren teren = (MapiranjeBaze_Teren) comboBoxTeren.getSelectedItem();
-						int idTeren_sifra = teren.getTeren_sifra();
-						
+						String Novi_termin_pocetak = textField_termin_pocetak.getText();
+						String Novi_termin_trajanje = textField_termin_trajanje.getText();
 
-				
-						if (!(Nova_aktivnost_naziv.equals(""))) {
+
+						
+						if (!(Novi_termin_pocetak.equals("") || Novi_termin_trajanje.equals(""))) {
 
 							try {						
 							 	  Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
 							 	 Connection conn = DriverManager.getConnection ("jdbc:mysql://ucka.veleri.hr/dtrbovic?" + "user=dtrbovic&password=11");
-								  //id, naziv, teren_sifra
-								  String sql = "UPDATE RDA_proj_Aktivnost SET aktivnost_naziv=?, teren_sifra=? WHERE aktivnost_sifra=?;";
+								  //id, pocetak, trajanje
+								  String sql = "UPDATE RDA_proj_Termin SET termin_pocetak_sati=?, termin_trajanje_sati=? WHERE termin_redni_broj=?;";
 				
 								  PreparedStatement stmt = conn.prepareStatement(sql);
-								  stmt.setString(1, Nova_aktivnost_naziv);
-								  stmt.setInt(2, idTeren_sifra);
-								  stmt.setInt(3, idAktivnost_sifra);
-
+								  stmt.setString(1, Novi_termin_pocetak);
+								  stmt.setString(2, Novi_termin_trajanje);
+								  stmt.setInt(3, idTermin_redni_broj);
 
 					  			  stmt.execute();
 												
 								  conn.close();
 											
-								  textField_aktivnost_naziv.setText("");			/*prazni polja nakon unosa*/
+								  textField_termin_pocetak.setText("");			/*prazni polja nakon unosa*/
+								  textField_termin_trajanje.setText("");
+
 
 
 							} 
@@ -144,7 +143,7 @@ public class DLG_Izmjena_Aktivnost extends JDialog {
 								*/
 								return;
 							}
-							System.out.println(idAktivnost_sifra + " " + Nova_aktivnost_naziv + " " + idTeren_sifra);
+							System.out.println(idTermin_redni_broj + " " + Novi_termin_pocetak + " " + Novi_termin_trajanje);
 							
 							
 						}
@@ -168,14 +167,11 @@ public class DLG_Izmjena_Aktivnost extends JDialog {
 			}
 			
 		}
-		CitanjeBaze_Teren citanjeBaze_Teren = new CitanjeBaze_Teren();
-		citanjeBaze_Teren.dohvatiTerene(comboBoxTeren);
-		
-		CitanjeBaze_Aktivnost citanjeBaze_Aktivnost = new CitanjeBaze_Aktivnost();
-		citanjeBaze_Aktivnost.dohvatiAktivnosti(comboBoxAktivnost);
-		
+		CitanjeBaze_Termin citanjeBaze_Termin = new CitanjeBaze_Termin();
+		citanjeBaze_Termin.dohvatiTermine(comboBoxTermin);
 		
 	}
+
 }
 
 
